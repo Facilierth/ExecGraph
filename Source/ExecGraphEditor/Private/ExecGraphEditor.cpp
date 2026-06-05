cfb7fa88e2ece5da90d068a1d87c0a7ebd7dee6c
Katarzyna Jaroszynska <katri.jaroszynska@gmail.com>
2026-06-05 11:54:26 +0200

    chore(setup): initialize plugin and all module scaffolds
    Create the ExecGraph.uplugin and establish the three-tier module architecture: ExecGraphRuntime, ExecGraph, and ExecGraphEditor.

---
+++ Source/ExecGraphEditor/Private/ExecGraphEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "ExecGraphEditor.h"
+
+#define LOCTEXT_NAMESPACE "FExecGraphEditorModule"
+
+void FExecGraphEditorModule::StartupModule()
+{
+    
+}
+
+void FExecGraphEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FExecGraphEditorModule, ExecGraphEditor)
---
+++ Source/ExecGraphRuntime/Private/ExecGraphRuntime.cpp
@@ -0,0 +1,17 @@
+﻿#include "ExecGraphRuntime.h"
+
+#define LOCTEXT_NAMESPACE "FExecGraphRuntimeModule"
+
+void FExecGraphRuntimeModule::StartupModule()
+{
+    
+}
+
+void FExecGraphRuntimeModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FExecGraphRuntimeModule, ExecGraphRuntime)
