3cb3b32aa369e21d7b21a046ac069875716b3b04
Katarzyna Jaroszynska <katri.jaroszynska@gmail.com>
2026-06-05 15:14:16 +0200

    feat(data): implement graph asset wrapper
    - Create the UExecGraphAsset data asset.
    - Implement the StartGraphInstance function to instantiate the UExecContext, push the first Token entry into the queue, and start the VM.

---
+++ Source/ExecGraph/Private/ExecGraphAsset.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ExecGraphAsset.h"
+#include "Nodes/ExecNodeBase.h"
+#include "ExecContext.h"
+#include "Engine/World.h"
+
+UExecContext* UExecGraphAsset::StartGraphInstance(UObject* OwnerContext, UExecStateSubsystem* StateSubsystem)
+{
+	if (!EntryNode || !OwnerContext || !StateSubsystem) return nullptr;
+	
+	UExecContext* Context = NewObject<UExecContext>(OwnerContext);
+	Context->InitializeContext(OwnerContext, StateSubsystem);
+	
+	FExecToken StartToken;
+	
+	Context->PushExecution(StartToken, EntryNode, FName("Start"));
+	Context->ProcessExecutionQueue();
+
+	return Context;
+}
---
+++ Source/ExecGraph/Public/ExecGraphAsset.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ExecGraphAsset.generated.h"
+
+class UExecNodeBase;
+class UExecContext;
+
+UCLASS(BlueprintType, Category = "ExecGraph")
+class EXECGRAPH_API UExecGraphAsset : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere, Category = "Graph Data")
+	UExecNodeBase* EntryNode;
+
+	UPROPERTY(VisibleAnywhere, Category = "Graph Data")
+	TArray<UExecNodeBase*> AllNodes;
+
+	// Spawns a runtime execution context and starts the graph "pulse"
+	UFUNCTION(BlueprintCallable, Category = "ExecGraph|Runtime")
+	UExecContext* StartGraphInstance(UObject* OwnerContext, UExecStateSubsystem* StateSubsystem);
+};
